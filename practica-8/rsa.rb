12d9a0525e6a8fbeb64fe01659cb0b754865d576
Carlos de Armas <alu0100816167@ull.edu.es>
2017-05-08 21:17:30 +0100

    Funcionando hasta encriptar

--- practica-8/rsa.rb
+++ practica-8/rsa.rb
@@ -1,47 +1,48 @@
-def fast_expo(base, expo, modulo)
+class Integer
+  def fast_expo(b, m)
     # b -> exponente
     # m -> modulo
     x = 1
-    y = base % modulo
-    while expo > 0 && y > 1
-        if expo.odd?
-            x = (x * y) % modulo
-            expo -= 1
-        else
-            y *= y % modulo
-            expo /= 2
-        end
+    y = self % m
+    while b > 0 && y > 1
+      if b.odd?
+        x = (x * y) % m
+        b -= 1
+      else
+        y *= y % m
+        b /= 2
+      end
     end
     x
+  end
 end
- 
+
 def gcd_euclides(a, b)
-   while b != 0
-       t = b
-       b = a % b
-       a = t
-   end
-   a
+  while b != 0
+    t = b
+    b = a % b
+    a = t
+  end
+  a
 end
- 
+
 # para calcular "e", que es el inverso de d modulo Φ(n)
 # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Computing_multiplicative_inverses_in_modular_structures
+# recibe d y phi como parámetro, devuelve el valor de e
 def modular_inverse(a, n)
-    t = 0
-    r = n
-    newt = 1
-    newr = a
-        
-    while newr != 0
-        quotient = r / newr
-        t, newt = newt, t - quotient * newt
-        r, newr = newr, r - quotient * newr
-    end
-    return false if r > 1
-    if t < 0 
-        t += n
-    end
-    t
+  t = 0
+  r = n
+  newt = 1
+  newr = a
+
+  while newr != 0
+    quotient = r / newr
+    t, newt = newt, t - quotient * newt
+    r, newr = newr, r - quotient * newr
+  end
+  return false if r > 1
+  t += n if t < 0
+  t
 end
 
 # Lehman primality test
@@ -55,51 +56,177 @@ end
 # 4. Loop for l times, keep trying different random values of a.
 # 5. If any element in array is not equal to 1, return n as composite. Otherwise, n is prime.
 def lehmann_test(n, l)
-    b = []
-    for it in 1..l
-        a = Random.rand(1..n-1)
-        c = a**((n-1)/2) % n
-        if c != 1 && c != n - 1
-            return 1
-        else
-            b.insert(it, c)
-        end
+  b = []
+  for it in 1..l
+    a = Random.rand(1..n - 1)
+    c = a**((n - 1) / 2) % n
+    if c != 1 && c != n - 1
+      return 1
+    else
+      b.insert(it, c)
     end
-    
-    if b.all? { |num| num == 1 } == true
-        return 1
-    end
-    return 0
+  end
+
+  return 1 if b.all? { |num| num == 1 } == true
+  0
 end
 
 def require_data
-    prime_p = 0 # privado
-    prime_q = 0 # privado
-    d = 0
-    phi = 0
-    loop do 
-        puts "Introduce el número p, tiene que ser primo"
-        prime_p = gets.to_i
-    break if lehmann_test(prime_p, 100) == 1
-    end
-    
-    loop do 
-        puts "Introduce el número q, tiene que ser primo"
-        prime_q = gets.to_i
-    break if lehmann_test(prime_q, 100) == 1
-    end
-    
-    phi = (prime_p - 1) * (prime_q - 1)
-    n = prime_p * prime_q # publica
-    
-    loop do
-        puts "Introduce el valor de d, tal que sea comprimo con Φ(n)= #{phi}"
-        d = gets.to_i
-        break if gcd_euclides(d, phi) == 1
-    end
-    
-    
-    fs_object = FiatShamir.new(prime_p, prime_q, s, iter)
-    # return fs_object
+  prime_p = 0 # privado
+  prime_q = 0 # privado
+  d = 0
+  phi = 0
+  loop do
+    puts 'Introduce el número p, tiene que ser primo'
+    prime_p = gets.to_i
+    break if lehmann_test(prime_p, 100).zero?
+  end
+
+  loop do
+    puts 'Introduce el número q, tiene que ser primo'
+    prime_q = gets.to_i
+    break if lehmann_test(prime_q, 100).zero?
+  end
+
+  phi = (prime_p - 1) * (prime_q - 1)
+  n = prime_p * prime_q # publica
+
+  loop do
+    puts "Introduce el valor de d, tal que sea comprimo con Φ(n)= #{phi}"
+    d = gets.to_i
+    break if gcd_euclides(d, phi) == 1
+  end
+  p
+  puts 'Introduce el mensaje:'
+  msg = gets.chomp
+  rsa_object = RSA.new(prime_p, prime_q, d, phi, msg)
+  # return fs_object
 end
-    
+
+class RSA
+  attr_accessor :p, :q, :d, :phi, :n, :e, :message, :numeric_encrypt, :final_encrypt, :decrypted, :block_size
+  def initialize(prime_p, q, d, msg)
+    @p = prime_p # inf privada
+    @q = q # inf privada
+    @d = d # inf privada
+    @phi = (@p - 1) * (@q - 1) # inf privada
+    @n = @p * @q # inf publica
+    @e = modular_inverse(@d, @phi) # inf publica
+    @message = msg.delete(' ') # al mensaje le quitamos los espacios en blanco
+    @numeric_encrypt = [] # aquí va el cifrado numérico
+    @final_encrypt = [] # cifrado final
+    @decrypted = []
+    @arr = [] # array de indices
+    @LETTER_TO_INDEX = { 'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7,
+
+                         'I' => 8, 'J' => 9, 'K' => 10, 'L' => 11, 'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15, 'Q' => 16, 'R' => 17, 'S' => 18,
+
+                         'T' => 19, 'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23, 'Y' => 24, 'Z' => 25 }.freeze
+    @block_size = set_block_size
+  end
+
+  def to_s
+    puts 'Información privada'
+    puts "p = #{@p}, q = #{@q}, d = #{@d}, Φ(n) = #{@phi}"
+    puts
+    puts 'Información pública'
+    puts "n = #{@n}, e = #{@e}"
+    puts
+    puts "Tamaño del bloque = #{@block_size}"
+    puts
+  end
+
+  def finder(str)
+    @LETTER_TO_INDEX[str] || @LETTER_TO_INDEX.key(str)
+  end
+
+  def set_block_size
+    base = @LETTER_TO_INDEX.length
+    j = 0
+    j += 1 while base**j < @n
+    j - 1
+  end
+
+  def modular_inverse(a, n)
+    t = 0
+    r = n
+    newt = 1
+    newr = a
+
+    while newr != 0
+      quotient = r / newr
+      t, newt = newt, t - quotient * newt
+      r, newr = newr, r - quotient * newr
+    end
+    return false if r > 1
+    t += n if t < 0
+    t
+  end
+
+  def numeric_cypher
+    # arr = [] # aqui se van a almacenar los indices de las letras
+    @message.each_char do |char|
+      index_of_letter = finder(char)
+      @arr.push(index_of_letter)
+    end
+    puts "Vector de índices de las letras del mensaje #{@arr.inspect}"
+    base = @LETTER_TO_INDEX.length # tamaño del alfabeto (es la base de la codificacion numerica)
+    vec = @arr.each_slice(@block_size).to_a # PARTIMOS EL VECTOR SEGUN EL TAMAÑO DEL BLOQUE
+    vec.each do |item|
+      t = 0
+      for it in 0...item.length
+        # tuve que separarlo mucho para que los resultados fueran correctos
+        # p "#{item[it]} * #{base}} elevado a #{(item.length-1)-it}"
+        iter = (item.length - 1) - it
+        ex = base**iter
+        mul = item[it]
+        v = mul * ex
+        t += v
+      end
+      @numeric_encrypt.push(t)
+    end
+    puts
+    puts "Resultado de la codificación numérica: #{@numeric_encrypt}"
+    puts
+  end
+
+  def encrypt
+    @numeric_encrypt.each do |item|
+      cipher = item.fast_expo(@e, @n)
+      @final_encrypt.push(cipher)
+    end
+
+    puts "El cifrado final es: #{@final_encrypt}"
+    puts
+  end
+
+  def decto26(int)
+    tam = @block_size
+    b = []
+    i = 0
+    while i < tam
+      r = int % 26
+      int /= 26
+      b[tam - i - 1] = @decrypted[r]
+      i += 1
+    end
+    puts b.to_s
+end
+
+  def decrypt
+    @final_encrypt.each do |element|
+      m_i = element.fast_expo(@d, @n)
+      @decrypted.push(m_i)
+    end
+
+    puts "El mensaje descifrado en índices es: #{@decrypted}"
+  end
+end
+
+msg_test = 'MANDA DINEROS'
+prueba = RSA.new(421, 7, 1619, msg_test)
+prueba.to_s
+prueba.numeric_cypher
+prueba.encrypt
+prueba.decrypt
+# prueba.decto26(10)
--- practica-8/test.rb
+++ practica-8/test.rb
@@ -1,57 +1,56 @@
 class Integer
-    def fast_expo(b, m)
-        # b -> exponente
-        # m -> modulo
-        x = 1
-        y = self % m
-        while b > 0 && y > 1
-            if b.odd?
-                x = (x * y) % m
-                b -= 1
-            else
-                y *= y % m
-                b /= 2
-            end
-        end
-        x
-    end
-end
-
-def fast_expo(base, exponente, modulo)
+  def fast_expo(b, m)
     # b -> exponente
     # m -> modulo
     x = 1
-    y = base % modulo
-    while exponente > 0 && y > 1
-        if exponente.odd?
-            x = (x * y) % modulo
-            exponente -= 1
-        else
-            y *= y % modulo
-            exponente /= 2
-        end
+    y = self % m
+    while b > 0 && y > 1
+      if b.odd?
+        x = (x * y) % m
+        b -= 1
+      else
+        y *= y % m
+        b /= 2
+      end
     end
     x
+  end
+end
+
+def fast_expo(base, exponente, modulo)
+  # b -> exponente
+  # m -> modulo
+  x = 1
+  y = base % modulo
+  while exponente > 0 && y > 1
+    if exponente.odd?
+      x = (x * y) % modulo
+      exponente -= 1
+    else
+      y *= y % modulo
+      exponente /= 2
+    end
+  end
+  x
 end
 
 # para calcular "e", que es el inverso de d modulo Φ(n)
 # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Computing_multiplicative_inverses_in_modular_structures
 def modular_inverse(a, n)
-    t = 0
-    r = n
-    newt = 1
-    newr = a
-        
-    while newr != 0
-        quotient = r / newr
-        t, newt = newt, t - quotient * newt
-        r, newr = newr, r - quotient * newr
-    end
-    return false if r > 1
-    if t < 0 
-        t += n
-    end
-    t
+  t = 0
+  r = n
+  newt = 1
+  newr = a
+
+  while newr != 0
+    quotient = r / newr
+    p quotient
+    t, newt = newt, t - quotient * newt
+    r, newr = newr, r - quotient * newr
+  end
+  return false if r > 1
+  t += n if t < 0
+  t
 end
 
 # i = modular_inverse(1619, 2520)
@@ -68,38 +67,81 @@ end
 # 4. Loop for l times, keep trying different random values of a.
 # 5. If any element in array is not equal to 1, return n as composite. Otherwise, n is prime.
 def lehmann(n, l)
-    b = []
-    for it in 1..l
-        a = Random.rand(1..n-1)
-        c = a**((n-1)/2) % n
-        if c != 1 && c != n - 1
-            return 1
-        else
-            b.insert(it, c)
-        end
+  b = []
+  for it in 1..l
+    a = Random.rand(1..n - 1)
+    c = a**((n - 1) / 2) % n
+    if c != 1 && c != n - 1
+      return 1
+    else
+      b.insert(it, c)
     end
-    
-    if b.all? { |num| num == 1 } == true
-        return 1
-    end
-    return 0
+  end
+
+  return 1 if b.all? { |num| num == 1 } == true
+  0
 end
 
 def gcd_euclides(a, b)
-    while b != 0
-        t = b
-        b = a % b
-        a = t
-    end
-    a
+  while b != 0
+    t = b
+    b = a % b
+    a = t
+  end
+  a
 end
 
-p num = gcd_euclides(6, 35)
+LETTER_TO_INDEX = { 'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7,
 
+                    'I' => 8, 'J' => 9, 'K' => 10, 'L' => 11, 'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15, 'Q' => 16, 'R' => 17, 'S' => 18,
 
+                    'T' => 19, 'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23, 'Y' => 24, 'Z' => 25 }.freeze
 
+def finder(hash, str)
+  hash[str] || hash.key(str)
+end
 
+# msg = "MANDADINEROS"
+# arr = []
+# msg.each_char do |char|
+#     index_of_letter = finder(LETTER_TO_INDEX, char)
+#     arr.push(index_of_letter)
+# end
+#  p arr
+#
+#  base = LETTER_TO_INDEX.length
+#  tam = 2
+#  vec = arr.each_slice(tam).to_a
+#
+#  nuevo = []
+#
+#  # 0*26^3+1*26^2+2*26+3=731
+#  vec.each { |item|
+#      valor = 0
+#      t = 0
+#      for it in 0...item.length
+#          #valor = item[it] * (base**(item.length-1)-it)
+#          p "#{item[it]} * #{base}} elevado a #{(item.length-1)-it}"
+#          iter = (item.length-1)-it
+#          ex = base ** iter
+#          mul = item[it]
+#          v = mul * ex
+#          t += v
+#          #p t
+#          puts
+#      end
+#      nuevo.push(t)
+#
+#  }
+#  p nuevo
+#
 
-
-
-
+def set_block_size
+  base = 26
+  j = 0
+  n = 2947
+  j += 1 while base**j < n
+  j - 1
+end
+res = set_block_size
+p res
